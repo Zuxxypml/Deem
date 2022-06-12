e892fb0dcc47aa4beea7b096a60eb53e21a09049
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-06-12 08:40:54 +0100

    Working on Dashboard Dashboard page 25%

--- public/Assets/js/deem-dashboard.js
+++ public/Assets/js/deem-dashboard.js
@@ -1,24 +1,32 @@
 const body = document.querySelector("body"),
-  sidebar = body.querySelector("nav"),
-  toggle = body.querySelector(".toggle"),
-  searchBtn = body.querySelector(".search-box"),
-  modeSwitch = body.querySelector(".toggle-switch"),
-  modeText = body.querySelector(".mode-text");
+  modeToggle = body.querySelector(".mode-toggle");
+sidebar = body.querySelector("nav");
+sidebarToggle = body.querySelector(".sidebar-toggle");
 
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
-});
-
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.remove("close");
-});
-
-modeSwitch.addEventListener("click", () => {
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
   body.classList.toggle("dark");
+}
 
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+  sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
   if (body.classList.contains("dark")) {
-    modeText.innerText = "Light mode";
+    localStorage.setItem("mode", "dark");
   } else {
-    modeText.innerText = "Dark mode";
+    localStorage.setItem("mode", "light");
+  }
+});
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
   }
 });
