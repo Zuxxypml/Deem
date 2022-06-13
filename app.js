4a569f3d048d9ce97b293fbc7508e4d4b4e9c6ff
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-06-13 18:08:02 +0100

    Added live value to dashboard

--- app.js
+++ app.js
@@ -257,7 +257,32 @@ app.get(
 // Homepage Handler
 app.route("/deem-home").get((req, res) => {
   if (req.isAuthenticated()) {
-    res.render("deem-dashboard");
+    Data().then((d) => {
+      console.log(d);
+      let { BTC, ETH, LTC, BNB, USDC, RPE, BCH, TTH, DOGE, TRON } = coins();
+
+      res.render("deem-dashboard", {
+        btcvalue: BTC,
+        ethvalue: ETH,
+        bchvalue: BCH,
+        usdcvalue: USDC,
+        dogevalue: DOGE,
+      });
+
+      function coins() {
+        let BTC = d.bitcoin.usd,
+          ETH = d.ethereum.usd,
+          LTC = d.litecoin.usd,
+          BNB = d.binancecoin.usd,
+          USDC = d["usd-coin"].usd,
+          RPE = d.ripple.usd,
+          BCH = d["bitcoin-cash"].usd,
+          TTH = d.tether.usd,
+          DOGE = d.dogecoin.usd,
+          TRON = d.tron.usd;
+        return { BTC, ETH, LTC, BNB, USDC, RPE, BCH, TTH, DOGE, TRON };
+      }
+    });
   } else {
     res.redirect("/login");
   }
